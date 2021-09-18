73cc52076ab51ced0dbf2195202d38c949f86705
Evan Mitchell <43766252+evanmitchell777@users.noreply.github.com>
2021-09-18 11:31:06 -0500

    Update PA03.py

--- PA03.py
+++ PA03.py
@@ -1,6 +1,8 @@
 from collections import Counter 
 list1=['b','a','c','b','a','c']
-bruh = (Counter(list1))
+def count():
+  bruh = (Counter(list1))
+
 print (sorted(bruh))
 print (sorted(bruh.items()))
 
